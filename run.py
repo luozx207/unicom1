4067b26b5b5238946f9970709072287f8e73bf60
Zixi Luo <zixiluo@MacBook-Pro-2.local>
2018-07-22 21:50:48 +0800

    POST接收数据类型改为json；数据库中添加了题目

--- run.py
+++ run.py
@@ -1,31 +1,58 @@
 #-*-coding:utf-8-*-
-from flask import Flask, g, flash, redirect, url_for,jsonify
+from flask import Flask, g, flash, redirect, url_for,jsonify,json
 from flask import render_template
 from flask import request,session
-from server import get_user_question,update_user_question
+from server import get_user_question,update_user_question,get_done
+from server import update_question
 
 app=Flask(__name__)
 
-@app.route('/question/<user_id>',methods=['GET','POST'])
-def question(user_id):
+@app.route('/question/type=<q_type>/<user_id>',methods=['GET','POST'])
+def question(user_id,q_type):
     if request.method=='POST':
+        data=json.loads(request.get_data())
         try:
-            update_user_question(**request.form)
+            update_user_question(**data)
         except Exception as e:
-            return e
-        return jsonify('seccess')
+            return jsonify({'status':0,'Exception':str(e)})
+        return jsonify({'status':1})
 
-    question_id,data,undone = get_user_question(user_id)
-    return jsonify({'questionId':question_id,
-                    'title':data[0],
-                    'options':[data[1],data[2],data[3],data[4]],
-                    'attributes':{
-                        'questionID':question_id,
-                        'answer':data[6],
-                        'answerExplain':data[5],
+    try:
+        question_id,data,undone = get_user_question(user_id,q_type)
+    except Exception as e:
+        return jsonify({'status':0,'Exception':str(e)})
+    return jsonify({'status':1,
+                    'data':{
+                        'questionId':question_id,
+                        'title':data[0],
+                        'options':[data[1],data[2],data[3],data[4]],
+                        'attributes':{
+                            'questionID':question_id,
+                            'answer':data[6],
+                            'answerExplain':data[5],
+                            },
+                        'totalQuestionNumber':undone,
                         },
-                    'totalQuestionNumber':undone,
                     })
+
+@app.route('/historyquestion/<user_id>')
+def historyquestion(user_id):
+    try:
+        data=get_done(user_id)
+    except Exception as e:
+        return jsonify({'status':0,'Exception':str(e)})
+    return jsonify({'status':1,
+                    'data':data,
+                    })
+
+@app.route('/updatequestion/',methods=['POST'])
+def updatequestion():
+    data=json.loads(request.get_data())
+    try:
+        update_question(data)
+    except Exception as e:
+        return jsonify({'status':0,'Exception':str(e)})
+    return jsonify({'status':1})
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
--- server.py
+++ server.py
@@ -8,17 +8,17 @@ def update_user_question(**data):
     cursor = conn.cursor()
     cursor.execute('insert into user_questions (user_id,question_id,result,\
                 choice) values(?,?,?,?)',\
-                    (data['userId'][0],data['questionId'][0],\
-                    data['answerResult'][0],data['userChoice'][0]))
+                    (data['userId'],data['questionId'],\
+                    data['answerResult'],data['userChoice']))
     conn.commit()
     conn.close()
 
-def get_user_question(user_id):
+def get_user_question(user_id,q_type):
     conn = sqlite3.connect('test.sqlite')
     cursor = conn.cursor()
     cursor.execute('select question_id from user_questions where user_id=?',(user_id,))
     done=[x[0] for x in cursor.fetchall()]
-    cursor.execute('select id from questions')
+    cursor.execute('select id from questions where q_type=?',(q_type,))
     questions= [x[0] for x in cursor.fetchall()]
     conn.close()
     undone=[]
@@ -32,7 +32,39 @@ def get_user_question(user_id):
 def get_question(question_id):
     conn = sqlite3.connect('test.sqlite')
     cursor = conn.cursor()
-    cursor.execute('select title,A,B,C,D,explain,answer from questions where id=?',(question_id,))
+    cursor.execute('select title,A,B,C,D,explain,answer,q_type from questions where id=?',(question_id,))
     data,=cursor.fetchall()
     conn.close()
     return data
+
+def get_done(user_id):
+    conn = sqlite3.connect('test.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('select question_id,choice from user_questions where user_id=?',(user_id,))
+    done=cursor.fetchall()
+    total=len(done)
+    question_data=[]
+    for d in done:
+        data = get_question(d[0])
+        question_data.append({'questionId':d[0],
+                        'q_type':data[7],
+                        'title':data[0],
+                        'options':[data[1],data[2],data[3],data[4]],
+                        'attributes':{
+                            'answer':data[6],
+                            'answerExplain':data[5],
+                            },
+                        'choice':d[1],
+                        })
+    return {'total':total, 'question_data':question_data}
+
+def update_question(data):
+    conn = sqlite3.connect('test.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('insert into questions (title,answer,A,B,C,D,\
+                explain,q_type) values(?,?,?,?,?,?,?,?)',\
+                    (data['title'],data['answer'],\
+                    data['A'],data['B'],data['C'],\
+                    data['D'],data['explain'],data['q_type']))
+    conn.commit()
+    conn.close()
