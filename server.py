8c6d9adbfb1ebf528977737cc915ed5089773138
Zixi Luo <zixiluo@MacBook-Pro-2.local>
2018-07-24 10:42:34 +0800

    查询未做题目该用差集方式

--- server.py
+++ server.py
@@ -17,14 +17,11 @@ def get_user_question(user_id,q_type):
     conn = sqlite3.connect('test.sqlite')
     cursor = conn.cursor()
     cursor.execute('select question_id from user_questions where user_id=?',(user_id,))
-    done=[x[0] for x in cursor.fetchall()]
+    done=set([x[0] for x in cursor.fetchall()])
     cursor.execute('select id from questions where q_type=?',(q_type,))
-    questions= [x[0] for x in cursor.fetchall()]
+    questions= set([x[0] for x in cursor.fetchall()])
     conn.close()
-    undone=[]
-    for q in questions:
-        if q not in done:
-            undone.append(q)
+    undone=list(questions-done)
     r=random.choice(undone)
     return [r,get_question(r),len(undone)-1]
 
